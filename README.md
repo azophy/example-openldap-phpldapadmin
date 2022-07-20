6aae7144a3dd6b052ddfe133dc9aba52ec0e2888
Abdurrahman Shofy Adianto <azophy@gmail.com>
2022-07-20 16:51:44 +0700

    Setup keycloak & openldap with example settings

--- README.md
+++ README.md
@@ -8,12 +8,19 @@ Experiment deploying OpenLDAP with PhpLdapAdmin
 2. cd to the folder then run `docker compose up`
 3. Setup:
   - import example LDAP entries:
-  - setup example keycloak realm & client:
     ```
-    docker compose exec keycloak sh /app/keycloak-autosetup-example.sh
+    docker compose exec openldap sh /app/load-seed-ldif.sh
+    ```
+    this repo included some example users and groups you could try:
+    - username: coba1 , password: coba1
+    - username: coba2 , password: coba2
+  - setup keycloak:
+    you need to setup realm, client, and user federation for ldap. or you could use the script to set example settings:
+    ```
+    docker compose exec keycloak sh /app/keycloak-autosetup-script.sh
 
     ```
-3. Website is up:
+4. Website is up:
   - PhpLdapAdmin available at http://localhost:8080 . Default login:
     > Login DN: "cn=admin,dc=example,dc=org"
     > Password: admin
@@ -21,6 +28,7 @@ Experiment deploying OpenLDAP with PhpLdapAdmin
     > user: admin
     > Password: admin
   - Sample FE client available at http://localhost:8000 . Use login provided by LDAP
+
 ## adapted from
 - https://github.com/Ramhm/openldap/blob/master/docker-compose.yml
 - https://github.com/osixia/docker-phpLDAPadmin#openldap--phpldapadmin-in-1
--- keycloak/keycloak-autosetup-script.sh
+++ keycloak/keycloak-autosetup-script.sh
@@ -31,3 +31,117 @@ $KCADM create clients -r ${REALM_NAME} \
 -s baseUrl="http://localhost:8000" \
 -s 'webOrigins=["*"]'
 
+# create user ldap provider
+$KCADM create components -r ${REALM_NAME}  -f - << EOF
+{
+  "name": "ldap",
+  "providerId": "ldap",
+  "providerType": "org.keycloak.storage.UserStorageProvider",
+  "parentId": "test_realm",
+  "config": {
+    "enabled": [
+      "true"
+    ],
+    "priority": [
+      "0"
+    ],
+    "fullSyncPeriod": [
+      "604800"
+    ],
+    "changedSyncPeriod": [
+      "86400"
+    ],
+    "cachePolicy": [
+      "DEFAULT"
+    ],
+    "evictionDay": [],
+    "evictionHour": [],
+    "evictionMinute": [],
+    "maxLifespan": [],
+    "batchSizeForSync": [
+      "1000"
+    ],
+    "editMode": [
+      "WRITABLE"
+    ],
+    "importEnabled": [
+      "true"
+    ],
+    "syncRegistrations": [
+      "true"
+    ],
+    "vendor": [
+      "other"
+    ],
+    "usePasswordModifyExtendedOp": [],
+    "usernameLDAPAttribute": [
+      "uid"
+    ],
+    "rdnLDAPAttribute": [
+      "uid"
+    ],
+    "uuidLDAPAttribute": [
+      "entryUUID"
+    ],
+    "userObjectClasses": [
+      "inetOrgPerson, organizationalPerson"
+    ],
+    "connectionUrl": [
+      "ldap://openldap:389"
+    ],
+    "usersDn": [
+      "ou=users,dc=example,dc=org"
+    ],
+    "authType": [
+      "simple"
+    ],
+    "startTls": [],
+    "bindDn": [
+      "cn=admin,dc=example,dc=org"
+    ],
+    "bindCredential": [
+      "admin"
+    ],
+    "customUserSearchFilter": [],
+    "searchScope": [
+      "1"
+    ],
+    "validatePasswordPolicy": [
+      "false"
+    ],
+    "trustEmail": [
+      "false"
+    ],
+    "useTruststoreSpi": [
+      "ldapsOnly"
+    ],
+    "connectionPooling": [
+      "true"
+    ],
+    "connectionPoolingAuthentication": [],
+    "connectionPoolingDebug": [],
+    "connectionPoolingInitSize": [],
+    "connectionPoolingMaxSize": [],
+    "connectionPoolingPrefSize": [],
+    "connectionPoolingProtocol": [],
+    "connectionPoolingTimeout": [],
+    "connectionTimeout": [],
+    "readTimeout": [],
+    "pagination": [
+      "true"
+    ],
+    "allowKerberosAuthentication": [
+      "false"
+    ],
+    "serverPrincipal": [],
+    "keyTab": [],
+    "kerberosRealm": [],
+    "debug": [
+      "false"
+    ],
+    "useKerberosForPasswordAuthentication": [
+      "false"
+    ]
+  }
+}
+EOF
